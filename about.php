8944fe845470fbab5101547846368d32e150824d
Anne-France Lambert <a.f.l@hotmail.fr>
2018-04-29 22:32:21 +0200

    page about

--- about.php
+++ about.php
@@ -2,14 +2,40 @@
     require_once('includes/header.php');
 ?>
 
-<div data-page="about" class="container-fluid">
-    <div class="containerAbout">
-        <h1>GL Organisation</h1>
+    <div data-page="about" class="container-fluid">
+        <div id="backgroundAbout">
+            <img class="img-responsive" width="1920" height="778" src="img/bg-about.jpg">
+        </div>
+        <div class="containerAbout container">
+            <h1>GL Organisation</h1>
+            <div class="accroche">
+                <p>Canvassing and identification of foreign producers for licencing contracts, sale or co-production, and tours of the show. </p>
+            </div>
+            <div class="row" id="content_about">
+                <div class="divContent" id="divRougeAbout" class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                    <p>GL Organisation's clients benefit from its network of producers and promoters all over the world.</p>
+                </div>
+                <div class="divContent" id="divTexteAbout" class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                    <p>GL Organisation is an established theatrical distribution company that provides an international circuit for booking and licensing shows in markets all over the world.</p>
+                    <p>International business experience includes developing, producing and promoting shows worldwide.<br>GL Organisation specializes in :</p>
+                    <ul>
+                        <li>Company management </li>
+                        <li>Marketing and strategy implementation </li>
+                        <li>Corporate financial structuring and management </li>
+                        <li>Tour business strategy </li>
+                        <li>Operational planning</li>
+                        <li>Financial modelling </li>
+                        <li>Budgeting </li>
+                        <li>Production general management </li>
+                        <li>Contract negotiation</li>
+                        <li> Legal affairs </li>
+                    </ul>
+                    <p>Founded in 2005 by Guillaume Lagorce, GL Organisation has represented shows such as : Notre-Dame de Paris, Romeo & Juliet, The Ten Commandments, The Sun King, The Little Prince, Ben-Hur stadium show, Tommy (The Who), Scooby Doo, Mozart the Rock Opera, 1789 Les amants de la Bastille, Mistinguett, The Legend of King Arthur, Summer 44, Oliver Twist, The Chorus…</p>
+                </div>
+            </div>
+        </div>
 
-        <p class="accroche">Canvassing and identification of foreign producers for licencing contracts, sale or co-production, and tours of the show. </p>
-    </div>
-</div>
 
-<?php
+        <?php
     require_once('includes/footer.php');
-?> 
+?>
