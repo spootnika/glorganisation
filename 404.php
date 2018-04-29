f9d841cd2df6a37d43104478f6e2cf4c5f0cb8b2
Anne-France Lambert <a.f.l@hotmail.fr>
2018-04-29 18:40:36 +0200

    page 404 version responsive

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
     require_once('includes/header.php');
 ?>
 
-    <div class="container" id="section404" data-page="page404">
+    <div class="container-fluid" id="section404" data-page="page404">
         <div id="content_page404">
             <h1>PAGE NOT FOUND</h1>
             <p>The page you requested was not found.<br> The link you used may be broken or you may have entered the URL incorrectly.</p>
