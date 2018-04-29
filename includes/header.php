285ab5895c87d0dcd293012937776de08224f5d1
spootnika <37499255+spootnika@users.noreply.github.com>
2018-04-29 15:43:15 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <!--JQuery-->
+        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.js"></script>
+
+        <!-- Bootstrap -->
+        <script src="js/bootstrap.min.js"></script>
+        <link href="css/bootstrap.css" rel="stylesheet">
+
+        <script src="js/scripts.js"></script>
+        <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
+
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+        <title>GL Organisation</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+    </head>
+
+    <body> 
+        <header>
+            <nav class="navbar navbar-info">
+                <div class="container-fluid site-nav">
+                    <div class="navbar-header">
+                        <!-- Logo -->
+                        <a href="index.php" class="navbar-brand">
+                            <div>
+                                <img src="img/logo_GL.png" alt="Logo GL Organisation">
+                            </div>
+                            <div>
+                                <span class="glo-title">
+                                    International Agency
+                                    <br><span class="highlight">for Live Shows</span>
+                                </span>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a class="classScript" href="index.php">Home</a>
+                            </li>
+                            <li>
+                                <a class="classScript" href="about.php">About</a>
+                            </li>
+                            <li>
+                                <a class="classScript" href="contact.php">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
