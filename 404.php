37bc9f97f919a84ac253b552bb4430bbd282a6dd
Anne-France Lambert <a.f.l@hotmail.fr>
2018-04-29 18:32:08 +0200

    page 404

--- 404.php
+++ 404.php
@@ -2,10 +2,14 @@
     require_once('includes/header.php');
 ?>
 
-<div class="container-fluid" data-page="page404">
+    <div class="container" id="section404" data-page="page404">
+        <div id="content_page404">
+            <h1>PAGE NOT FOUND</h1>
+            <p>The page you requested was not found.<br> The link you used may be broken or you may have entered the URL incorrectly.</p>
+            <p><a href="index.php" class="button">BACK HOMEPAGE</a></p>
+        </div>
+    </div>
 
-</div>
-
-<?php
+    <?php
     require_once('includes/footer.php');
-?> 
+?>
