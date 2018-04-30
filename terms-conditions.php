5b1e14e09ffa70323ede9aa5f0d23e1356b46e01
Spootnika <celia.lflr@gmail.com>
2018-04-30 12:44:23 +0200

    Début Terms

--- terms-conditions.php
+++ terms-conditions.php
@@ -3,7 +3,25 @@
 ?>
 
 <div class="container-fluid" data-page="terms">
+    <div class="containerTerms">
+        <h1>Terms & Conditions</h1>
 
+        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+
+        Hic quibusdam amet culpa debitis ipsam sed dolorum eveniet iste provident rem molestias quas tempora suscipit non iusto, dolorem pariatur perspiciatis exercitationem itaque quam assumenda dolore. Ducimus, ipsam deleniti tempora autem unde officia maxime. Expedita nemo odio ipsam nam dignissimos excepturi quo labore. Eius obcaecati itaque culpa, atque ut quisquam asperiores minima beatae, consequatur, laboriosam illo nisi! Doloribus aspernatur officiis ullam atque at voluptate nam nulla sequi harum quam nisi doloremque amet laboriosam, assumenda earum unde quis! Cupiditate, maiores quos libero nobis non mollitia praesentium nulla debitis doloremque aspernatur rerum aperiam deserunt optio fugit architecto modi dolorum commodi ut sequi?</p>
+
+        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+        
+        Hic quibusdam amet culpa debitis ipsam sed dolorum eveniet iste provident rem molestias quas tempora suscipit non iusto, dolorem pariatur perspiciatis exercitationem itaque quam assumenda dolore. Ducimus, ipsam deleniti tempora autem unde officia maxime. Expedita nemo odio ipsam nam dignissimos excepturi quo labore. Eius obcaecati itaque culpa, atque ut quisquam asperiores minima beatae, consequatur, laboriosam illo nisi! Doloribus aspernatur officiis ullam atque at voluptate nam nulla sequi harum quam nisi doloremque amet laboriosam, assumenda earum unde quis! Cupiditate, maiores quos libero nobis non mollitia praesentium nulla debitis doloremque aspernatur rerum aperiam deserunt optio fugit architecto modi dolorum commodi ut sequi?</p>
+
+        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+        
+        Hic quibusdam amet culpa debitis ipsam sed dolorum eveniet iste provident rem molestias quas tempora suscipit non iusto, dolorem pariatur perspiciatis exercitationem itaque quam assumenda dolore. Ducimus, ipsam deleniti tempora autem unde officia maxime. Expedita nemo odio ipsam nam dignissimos excepturi quo labore. Eius obcaecati itaque culpa, atque ut quisquam asperiores minima beatae, consequatur, laboriosam illo nisi! Doloribus aspernatur officiis ullam atque at voluptate nam nulla sequi harum quam nisi doloremque amet laboriosam, assumenda earum unde quis! Cupiditate, maiores quos libero nobis non mollitia praesentium nulla debitis doloremque aspernatur rerum aperiam deserunt optio fugit architecto modi dolorum commodi ut sequi?</p>
+
+        <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+        
+        Hic quibusdam amet culpa debitis ipsam sed dolorum eveniet iste provident rem molestias quas tempora suscipit non iusto, dolorem pariatur perspiciatis exercitationem itaque quam assumenda dolore. Ducimus, ipsam deleniti tempora autem unde officia maxime. Expedita nemo odio ipsam nam dignissimos excepturi quo labore. Eius obcaecati itaque culpa, atque ut quisquam asperiores minima beatae, consequatur, laboriosam illo nisi! Doloribus aspernatur officiis ullam atque at voluptate nam nulla sequi harum quam nisi doloremque amet laboriosam, assumenda earum unde quis! Cupiditate, maiores quos libero nobis non mollitia praesentium nulla debitis doloremque aspernatur rerum aperiam deserunt optio fugit architecto modi dolorum commodi ut sequi?</p>
+    </div>
 </div>
 
 <?php
